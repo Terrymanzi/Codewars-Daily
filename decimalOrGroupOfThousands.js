6e44af9ca2acf05e4eaf39bc3c74ff805cf5f6fc
terrymanzi <m.terry@alustudent.com>
2026-01-21 14:53:47 +0200

    feat: enhance sumUpNumbers function to improve separator detection and normalization

--- decimalOrGroupOfThousands.js
+++ decimalOrGroupOfThousands.js
@@ -1,18 +1,34 @@
 function sumUpNumbers(arr) {
   // your code here
   let newArr = [];
-  console.log(arr);
-  //     return '';
   for (let item of arr) {
-    //     if(item.contains(".")){
-    //     console.log()
-    //     }
-    console.log(item);
-    console.log(item.lastIndexOf("."));
+    //detect separator format
+    if (item[item.length - 3] === ".") {
+      console.log("US");
+      console.log(item);
 
-    //check separator
-    if (item.lastIndexOf(".") === -1) {
-      //do some transformations
+      //normalize separators
+      let newItem = item.replace(/,/g, "");
+      console.log(newItem);
+      newArr.push(Number(newItem));
+    } else {
+      console.log("UK");
+      console.log(item);
+
+      //normaliza separators
+      let newItem = item.replace(/\./g, "").replace(/,/g, ".");
+      console.log(newItem);
+      newArr.push(Number(newItem));
     }
   }
+  console.log(newArr);
+  let sum = newArr.reduce((accumulator, current) => {
+    return accumulator + current;
+  });
+
+  console.log(sum);
+  return sum.toLocaleString("en-US", {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
 }
